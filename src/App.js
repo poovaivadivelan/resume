0245f47720d2e249dc1f60b74c003148772adeff
Poovai Vadivelan Ramasamy <vadivel@Poovais-Laptop.local>
2024-02-18 23:06:06 +0530

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Document, Page, pdfjs } from 'react-pdf';
+import { PDFViewer } from '@react-pdf/renderer';
+import ReactPDF from '@react-pdf/renderer';
+import { ReactDOM } from 'react';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import resume from './resources/Poovaivadivelan.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Document file={resume} onLoadError={console.error} style={{width: '100vw', height:'auto'}}>
+        <Page pageIndex={0} renderAnnotationLayer={true}  renderTextLayer={false}></Page>
+      </Document>
     </div>
+
   );
 }
-
 export default App;
