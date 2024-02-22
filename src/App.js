ae53e8024daf0360cba8df972ea1b09e0d852406
Poovai Vadivelan Ramasamy <poovai.vadivelan@yahoo.in>
2024-02-23 01:01:54 +0530

    bugfix: fixed js render issue

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import { PDFViewer } from '@react-pdf/renderer';
 import ReactPDF from '@react-pdf/renderer';
 import { ReactDOM } from 'react';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import resume from './resources/Poovaivadivelan.pdf';
+import resume from './assets/Poovaivadivelan.pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
+const url = `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
+pdfjs.GlobalWorkerOptions.workerSrc = url
 
 
 function App() {
   return (
     <div>
       <Document file={resume} onLoadError={console.error} style={{width: '100vw', height:'auto'}}>
-        <Page pageIndex={0} renderAnnotationLayer={true}  renderTextLayer={false}></Page>
+        <Page pageIndex={0} renderAnnotationLayer={false}  renderTextLayer={false}></Page>
       </Document>
     </div>
 
