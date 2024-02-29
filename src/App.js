58f3f6e74dde2b1581b12959e2a7197d5e7186ec
Poovai Vadivelan Ramasamy <poovai.vadivelan@yahoo.in>
2024-02-29 09:49:55 +0530

    fix:icon fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Document, Page, pdfjs } from 'react-pdf';
-import { PDFViewer } from '@react-pdf/renderer';
+import { PDFViewer, View, Text } from '@react-pdf/renderer';
 import ReactPDF from '@react-pdf/renderer';
 import { ReactDOM } from 'react';
 import resume from './assets/puvi.pdf';
@@ -13,8 +13,10 @@ pdfjs.GlobalWorkerOptions.workerSrc = url
 function App() {
   return (
     <div>
-      <Document file={resume} onLoadError={console.error} style={{width: '100vw', height:'auto'}}>
-        <Page pageIndex={0} renderAnnotationLayer={false}  renderTextLayer={false}></Page>
+      <Document file={resume} onLoadError={console.error} >
+        <Page size="A4" pageIndex={0} renderAnnotationLayer={false}  renderTextLayer={false}></Page>
+        <View style={{ color: 'white', textAlign: 'center', margin: 30 }}>
+      </View>
       </Document>
     </div>
 
